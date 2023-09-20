3afa7d2680975a4ee5058a994f68e1f52d926dac
shahin1290 <s.patowary@yahoo.com>
2023-09-20 17:37:48 +0200

    Update design

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+    <section className="">
+      <div className="grid grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-6 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-6xl  lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
@@ -43,17 +43,17 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
+          className="col-span-6 justify-self-end pb-40"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+         
             <Image
-              src="/images/hero-image.png"
+              src="/hero.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className=""
+              width={500}
+              height={500}
             />
-          </div>
+         
         </motion.div>
       </div>
     </section>
